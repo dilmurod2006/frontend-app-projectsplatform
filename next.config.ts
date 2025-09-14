b9e449d05313017e653728cdac679ebc10dcb6cf
Amonov Dilmurod <115336811+dilmurod2006@users.noreply.github.com>
2025-09-14 05:23:37 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  // Rasmlar uchun sozlamalar
-  // trailingSlash: true,  
-  images: {
-    // Statik eksportda barcha rasmlar optimizatsiyasiz ishlatilsin
-    unoptimized: true,
-  },
-  // skipTrailingSlashRedirect: true,
-  distDir: 'dist',
+
 };
 
 export default nextConfig;
